073ce1207850a50158da012e2e70d97f4492c188
monk <monk@MacBook-Air-monk.local>
2017-04-21 15:14:36 +0300

    main page v0.1

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,22 @@
-/* Third party */
-//= ../../bower_components/jquery/dist/jquery.min.js
-
-/* Custom */
 //= partials/helper.js
+$(document).ready(function(){
+  $('.main-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    infinite: true,
+    dots: true,
+    speed: 300,
+    appendArrows: $(".main-slider-arrows"),
+    appendDots: $(".main-slider-dots")
+  });
+  $(".main-sidebar").easytabs({
+    animate: true,
+    animationSpeed: 100,
+    defaultTab: ".tab-btn:first-child",
+    panelActiveClass: "active-panel",
+    tabActiveClass: "active",
+    tabs: ".tab-btn",
+    updateHash: false
+  });
+});
